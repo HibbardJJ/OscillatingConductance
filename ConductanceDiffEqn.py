56b1f125eb478a458dc5694f12fdcc244084e765
HibbardJJ <31393066+HibbardJJ@users.noreply.github.com>
2017-12-07 00:53:26 -0700

    Second commit

--- ConductanceDiffEqn.py
+++ ConductanceDiffEqn.py
@@ -9,17 +9,18 @@ import numpy as np
 #Leaf Constants
 X = 1
 m = 2
-lambda_g = 500000000000000000000000000
+lambda_g = 50
 lambda_e = 60
 w_o = 657959000
 T_w = 4982.85
 sigma = 0.05
 R = 8.314
-c_g = 0.4
+c_g = 3088
 v_w = 1.8054197671114737e-05
-const = 2e6
+const = 2156
 const2 =0.5e6
 c_e = 0.4
+c_g_values=[]
 
 #Theoretical Conductance
 def g_s(t,w_a,T_e):
@@ -34,9 +35,23 @@ def g_s(t,w_a,T_e):
     return sigma*w_a + (1-sigma)*w_i(*T_e)"""
 #Pressure in the guard cell
 def P_g(t,w_a,T_e):
-    return ((R*T_e)/v_w)*np.log((sigma*w_a+(1-sigma)*w_o*np.exp(-T_w/(T_e+273)))/(w_o*np.exp(-T_w/(T_e+273)))) - c_g*R*T_e - (const/lambda_g)*np.exp(-lambda_g*t)
+    global c_g
+    p_g = ((R*(T_e+273))/v_w)*np.log((sigma*w_a+(1-sigma)*w_o*np.exp(-T_w/(T_e+273)))/(w_o*np.exp(-T_w/(T_e+273)))) + c_g*R*(T_e+273) + (const)*np.exp(-lambda_g*t)
+    if p_g < 0:
+        c_g+=1
+        P_g(t,w_a,T_e)
+    else:
+        return p_g
+        c_g_values.append(c_g)
+
 #Pressure in the epidermal cells
 def P_e(t,w_a,T_e):
-    return (c_e*R*T_e)/(1-R*X*m*(w_a - w_o*np.exp(-T_w/(T_e+271)))) - (lambda_g*np.exp(1)/lambda_e)*(1/(1-R*X*m*(w_a - w_o*np.exp(-T_w/(T_e+271)))))*((R*T_e/v_w)*np.log((sigma*w_a+(1-sigma)*w_o*np.exp(-T_w/(T_e+273)))/(w_o*np.exp(-T_w/(T_e+271)))) - c_g*R*T_e) + (const*lambda_e/(1-R*X*m*(w_a - w_o*np.exp(-T_w/(T_e+271)))*lambda_e*lambda_g-lambda_g**2))*np.exp((1-lambda_g*t)) + const2*np.exp(-(1-R*X*m*(w_a - w_o*np.exp(-T_w/(T_e+271)))*(lambda_e*t)))
+    return (c_e*R*(T_e+273))/(1-R*X*m*(w_a - w_o*np.exp(-T_w/(T_e+273)))) - (lambda_g*np.exp(1)/lambda_e)*(1/(1-R*X*m*(w_a - w_o*np.exp(-T_w/(T_e+273)))))*((R*(T_e+273)/v_w)*np.log((sigma*w_a+(1-sigma)*w_o*np.exp(-T_w/(T_e+273)))/(w_o*np.exp(-T_w/(T_e+273)))) - c_g*R*(T_e+273)) + (const*lambda_e/(1-R*X*m*(w_a - w_o*np.exp(-T_w/(T_e+273)))*lambda_e*lambda_g-lambda_g**2))*np.exp((1-lambda_g*t)) + const2*np.exp(-(1-R*X*m*(w_a - w_o*np.exp(-T_w/(T_e+273)))*(lambda_e*t)))
 
-print(P_g(0,25,296))
+def P_g1(t,w_a,T_e):
+    return ((R*(T_e+273))/v_w)*np.log((sigma*w_a+(1-sigma)*w_o*np.exp(-T_w/(T_e+273)))/(w_o*np.exp(-T_w/(T_e+273))))
+
+
+x=P_g1(1,20,23)
+y=P_g(1,19,24)
+print(y)
