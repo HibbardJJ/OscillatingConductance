e1be7cff228706e5249382de34e5e80ff34d7c4e
HibbardJJ <31393066+HibbardJJ@users.noreply.github.com>
2017-12-18 14:41:15 -0700

    Fifth Commit

--- ConductanceDiffEqn.py
+++ ConductanceDiffEqn.py
@@ -14,24 +14,24 @@ from scipy.optimize import curve_fit
 X = 1
 m = 2
 #Rate Constants--Remember, lambda_e > lambda_g
-#Best working value for lambda_g: 0.001
-lambda_g = 0.001
-#Best working value for lambda_e: 0.02
-lambda_e = 0.005
+#Best working value for lambda_g: 0.0001
+lambda_g = 0.000000001
+#Best working value for lambda_e: 0.0005
+lambda_e = 0.0000003
 
 w_o = 657959000
 T_w = 4982.85
 sigma = 0.05
 R = 8.314
-#Tentative c_g value: 1000
-c_g = 1000
+#Tentative c_g value: 0.01
+c_g = 0.01
 v_w = 1.8054197671114737e-05
-#const = 2e6
-const = 2e6
-#upper bound on const2 = 200000; lower bound = 0; best working value: 1
-const2 = 1000 - 10000
-#Best working value of c_e so far: 1.0e6
-c_e = 1.0e6
+#const = 1e6
+const = 1e3
+#upper bound on const2 = 200000; lower bound = 0; best working value: 10000
+const2 = 9
+#Best working value of c_e so far: 1.0e5...9.0e5
+c_e = 3.0e6
 
 
 #Data
@@ -60,21 +60,22 @@ initial_guess = [lambda_g, lambda_e, c_g, c_e, const, const2]
 
 def model_g_s(independent_variables, lambda_g, lambda_e, c_g, c_e, const, const2):
     t, w_a, T_e = independent_variables
-    return X*(P_g(t,w_a,T_e) - m*P_e(t,w_a,T_e))
+    #return X*(P_g(t,w_a,T_e) - m*P_e(t,w_a,T_e))
+    return (1/40)*g_s(t,w_a,T_e)
     
 #Curve-fit method--currently not working
-print(curve_fit(model_g_s, (plant_time, plant_w_a, plant_T_e), conductance, initial_guess))
+#print(curve_fit(model_g_s, (plant_time, plant_w_a, plant_T_e), conductance, initial_guess))
 
 
 dat=(g_s(plant_time,plant_w_a,plant_T_e))
-
+dat=dat*(1/40)
 plot.figure(2)
-plot.plot(plant_time,dat,time,conductance)
+plot.plot(plant_time,dat)
 plot.figure(3)
 plot.plot(time,conductance)
 plot.ylabel("Model Data")
 
-
+print(dat[0]-dat[4])
 #Theoretical Conductance
 def g_s(t,w_a,T_e):
     g_cell_pressure = P_g(t,w_a,T_e)
@@ -108,4 +109,5 @@ def l(w_a,T_e):
 
 #Pressure in the epidermal cells. Units: MPa
 def P_e(t,w_a,T_e):
-    return ((np.exp(l(w_a,T_e)*lambda_e*t))*(-np.exp(-t*(lambda_g + l(w_a,T_e)*lambda_e))*lambda_e*((-np.exp(lambda_g*t)*(d(T_e) + (b(w_a,T_e) + c(T_e))*f(w_a,T_e)))/(l(w_a,T_e)*lambda_e) + ((-5e6 + b(w_a,T_e) + c(T_e))*f(w_a,T_e))/(lambda_g + l(w_a,T_e)*lambda_e)) + const2))*(10e-6)
+    #return ((np.exp(l(w_a,T_e)*lambda_e*t))*(-np.exp(-t*(lambda_g + l(w_a,T_e)*lambda_e))*lambda_e*((-np.exp(lambda_g*t)*(d(T_e) + (b(w_a,T_e) + c(T_e))*f(w_a,T_e)))/(l(w_a,T_e)*lambda_e) + ((-5e6 + b(w_a,T_e) + c(T_e))*f(w_a,T_e))/(lambda_g + l(w_a,T_e)*lambda_e)) + const2))*(10e-6)
+    return ((lambda_g*(d(T_e) + b(w_a,T_e)*f(w_a,T_e) + c(T_e)*f(w_a,T_e) + l(w_a,T_e)*const2*np.exp(l(w_a,T_e)*t*lambda_e)) + lambda_e*l(w_a,T_e)*(d(T_e) + b(w_a,T_e)*f(w_a,T_e) +c(T_e)*f(w_a,T_e) + const*f(w_a,T_e)*np.exp(-lambda_g*t) + l(w_a,T_e)*const2*np.exp(l(w_a,T_e)*t*lambda_e)))/((lambda_g + l(w_a,T_e)*lambda_e)*l(w_a,T_e)))*(10e-6)
